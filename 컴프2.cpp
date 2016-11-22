c3e514bca6ad1467c4a6a1978822d6428474d94e
Kimbada1031 <qkxod@naver.com>
2016-11-23 01:38:56 +0900

    game1

---
+++ 컴프2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+cout << "카드 공격 게임" << endl << endl;
+cout << "1. PLAY" << endl << "2. HELP" << endl << "3. EXIT" << endl << endl;
+
+cout << "숫자를 입력하세요 : ";
+
+int start;
+cin >> start; 
+
+}
+
